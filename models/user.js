38f621295428332266813fd75e8335cd19354747
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-04-02 13:13:43 -0700

    added users

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const mongoose = require('mongose');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   fullname: { type: String, default: '' },
@@ -16,5 +17,13 @@ userSchema.set('toObject', {
     delete ret.password;
   }
 });
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+  
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+};
 
 module.exports = mongoose.model('User', userSchema);
